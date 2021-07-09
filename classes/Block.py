e77599f14b2ec169e5d12bd3d270e44fbfdd926d
ludovic <ludovic.angenard@orange.fr>
2021-07-09 03:58:30 +0200

    a part of block implementation

--- classes/Block.py
+++ classes/Block.py
@@ -1,27 +1,72 @@
+import json
+import os
+
 class Block:
 
     def __init__(
-            self, unique_id, hash,
-            parent_hash, transactions):
-        self.unique_id = unique_id
-        self.hash = hash
-        self.parent_hash = parent_hash
-        self.transactions = transactions
+            self, hash, parent_hash,
+            transactions, base_hash = 64):
+        self._hash = hash
+        self._parent_hash = parent_hash
+        self._base_hash = base_hash
+        self._transactions = transactions
 
     def check_hash(self):
-        pass
+        character_number = 0
+        for i in self._get_hash():
+            character_number += 1
+        if character_number == self._get_base_hash():
+            return True
+        else:
+            return False
 
     def add_transaction(self):
+        #wallet emetteur
+        #wallet recepteur
+        #montant
         pass
 
-    def get_transaction(self):
-        pass
+    def get_transaction(self, transaction_number):
+        #TODO complete this part
+        transaction = transaction_number
+        return transaction
 
     def get_weight(self):
-        pass
+        weight = os.stat("./content/blocs/{}.json".format(self.get_hash())).st_size
+        return weight
 
     def save(self):
-        pass
+        with open("./content/blocs/{}.json".format(self.get_hash()), 'w') as f:
+            data = {
+                "hash": self.get_hash(),
+                "base_hash": self.get_base_hash(),
+                "parent_hash": self.get_parent_hash(),
+                "base_hash": self.get_transactions(),
+            }
+            json.dump(data, f)
 
     def load(self):
-        pass
+        with open("./content/blocs/{}.json".format(self.get_hash())) as f:
+            data = json.load(f)
+        return data
+
+    def get_hash(self):
+        return self._hash
+
+    def get_parent_hash(self):
+        return self._parent_hash
+
+    def set_parent_hash(self, parent_hash):
+        self._parent_hash = parent_hash
+
+    def get_base_hash(self):
+        return self._base_hash
+
+    def set_base_hash(self, base_hash):
+        self._base_hash = base_hash
+
+    def get_transactions(self):
+        return self._transactions
+
+    def set_transactions(self, transactions):
+        self._transactions = transactions
