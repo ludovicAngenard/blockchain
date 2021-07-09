8c9e0d491b3e94c1fdd2c330d39b46e9d068afcd
ludovic <ludovic.angenard@orange.fr>
2021-07-09 14:10:44 +0200

    make private transaction's attributs

--- classes/Transaction.py
+++ classes/Transaction.py
@@ -2,15 +2,21 @@ class Transaction:
     def __init__(
             self, wallet_transmitter,
             wallet_emitter, price_number):
-        self.wallet_transmitter = wallet_transmitter
-        self.wallet_emitter = wallet_emitter
-        self.price_number = price_number
+        self._wallet_transmitter = wallet_transmitter
+        self._wallet_emitter = wallet_emitter
+        self._price_number = price_number
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self, id):
+        self._id = id
 
     def get_wallet_transmitter(self):
-        return self.wallet_transmitter
+        return self._wallet_transmitter
 
     def get_wallet_emitter(self):
-        return self.wallet_emitter
+        return self._wallet_emitter
 
     def get_price_number(self):
-        return self.price_number
+        return self._price_number
